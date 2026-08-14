eb61b08e338fdc3ec5e296f0a8e7c6af81b1ecc1
sarkanice <317065053+sarkanice@users.noreply.github.com>
2026-08-15 00:18:53 +0200

    Add Google Mobile Ads SDK

--- Package.swift
+++ Package.swift
@@ -1,18 +1,47 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "LabirintsGoogleAds",
-    platforms: [.iOS(.v15)],
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
-            name: "LabirintsGoogleAds",
-            targets: ["LabirintsGoogleAds"]
+            name: "GoogleMobileAds",
+            targets: ["GoogleMobileAdsTarget"]
+        ),
+        .library(
+            name: "GoogleUserMessagingPlatform",
+            targets: ["UserMessagingPlatformTarget"]
         )
     ],
     targets: [
         .target(
-            name: "LabirintsGoogleAds"
+            name: "GoogleMobileAdsTarget",
+            dependencies: [
+                .target(name: "GoogleMobileAds"),
+                .target(name: "UserMessagingPlatform")
+            ],
+            path: "GoogleMobileAdsTarget"
+        ),
+        .target(
+            name: "UserMessagingPlatformTarget",
+            dependencies: [
+                .target(name: "UserMessagingPlatform")
+            ],
+            path: "UserMessagingPlatformTarget",
+            linkerSettings: [
+                .linkedFramework("WebKit")
+            ]
+        ),
+        .binaryTarget(
+            name: "GoogleMobileAds",
+            path: "Binaries/GoogleMobileAds.xcframework"
+        ),
+        .binaryTarget(
+            name: "UserMessagingPlatform",
+            path: "Binaries/UserMessagingPlatform.xcframework"
         )
     ]
 )
---
+++ UserMessagingPlatformTarget/placeholder.swift
@@ -0,0 +1,1 @@
+// Wrapper target for UserMessagingPlatform binary.
