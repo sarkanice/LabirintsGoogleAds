7ac06c8ef05bb50b14f6dc74954c417863c0e64b
sarkanice <317065053+sarkanice@users.noreply.github.com>
2026-08-16 22:44:08 +0200

    Initialize Google Mobile Ads SDK

--- Sources/LabirintsGoogleAds/LabirintsGoogleAds.swift
+++ Sources/LabirintsGoogleAds/LabirintsGoogleAds.swift
@@ -1,5 +1,11 @@
 import Foundation
+import GoogleMobileAds
+import UserMessagingPlatform
 
 public enum LabirintsGoogleAds {
     public static let packageLoaded = true
+
+    public static func start() {
+        MobileAds.shared.start()
+    }
 }
